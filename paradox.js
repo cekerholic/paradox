522ffb6f160294c5dd4ae5fe692d9923b80b9517
Aris FM <masoglek@gmail.com>
2012-10-23 13:44:21 +0700

    initial commit

---
+++ paradox.js
@@ -0,0 +1,73 @@
+(function($) {
+
+	$.fn.parallax = function(options) {
+		
+		// All Parallax Options
+		var defaults = {
+			speed: 		700,
+			horizontal:	40,
+			vertical:	40,
+			bounce:		true,
+			repeat:		false,
+			fade:		false,
+			scroll:		false
+		};
+		
+		settings = $.extend({}, defaults, options);
+		
+		var $top = $(window).scrollTop();
+		
+		this.each(function() {
+			var $item = $(this);
+			$item.each(function animx(){
+			
+				// Fading Parallax onScroll
+				if (settings.scroll) {
+					$(window).scroll(function(){
+						var $elemtop = $(window).scrollTop();
+						if ( $top < $elemtop ) {
+							$item.animate({
+								left:settings.horizontal,
+								top:settings.vertical,
+								opacity:settings.fade ? 0 : 1
+							}, settings.speed)
+						}
+						else {
+							$item.animate({
+								left:0,
+								top:0,
+								opacity:1
+							}, settings.speed)
+						}
+						$top = $elemtop
+					});
+				}
+				
+				//Default Parallax
+				else {
+					$item.animate({
+						left:settings.horizontal,
+						top:settings.vertical,
+						opacity:settings.fade ? 0 : 1
+					}, settings.speed,
+					function() {
+						if (settings.bounce) {
+							$item.animate({
+								left:0,
+								top:0,
+								opacity:1
+							}, settings.speed,
+							function(){
+								if (settings.repeat) {
+									return animx()
+								}
+							})
+						}
+					});
+				}
+			});
+		});
+		return this;
+	};
+
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,34 @@
+# PARADOX: A Simple jQuery Parallax Plugin
+This a simple parallax plugin that rely on jQuery `.animate()`. Paradox work by simply move the element to any given position.
+
+### Basic CSS
+All your need to do is declare the position of your parallax, e.g. :
+	.parallax {
+		position: absolute;
+	}
+
+### Initialize Paradox
+	<script>
+		$('.parallax').parallax();
+	</script>
+
+### Available Options
+	<script>
+		$('.parallax').parallax({
+			speed: 		700, 	// (Number) Define parallax speed
+			horizontal:	40, 	// (Number) Define parallax horizontal movement
+			vertical:	40, 	// (Number) Define parallax vertical movement
+			bounce:		true,	// (Boolean) Enable bouncing effect
+			repeat:		false,	// (Boolean) Infinite animation
+			fade:		false,	// (Boolean) Fading animation
+			scroll:		false	// (Boolean) Start parallax after scroll is triggered
+		});
+	</script>
+
+### License 
+Licensed under [Creative Commons Attribution 3.0 Unported (CC BY 3.0)](http://creativecommons.org/licenses/by/3.0/)
+
+### Changelog
+* 1.0
+
+Initial release
